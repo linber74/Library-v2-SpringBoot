3b02084b1676a29a0032b6b5d6972b29b8c6583a
Linda Bergin <linber24@gmail.com>
2026-05-30 21:07:04 +0200

    Copied Book and changed to Spring Boot and a small change in Film

--- src/main/java/se/linda/library/model/entity/Film.java
+++ src/main/java/se/linda/library/model/entity/Film.java
@@ -2,11 +2,15 @@ package se.linda.library.model.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "film")
 @NoArgsConstructor
+@Getter
+@Setter
 public class Film extends VisualMedia {
 
     @Override
