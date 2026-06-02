bf39abbc42b1cab1de6f53a274de9f9e2556e467
Linda Bergin <linber24@gmail.com>
2026-06-02 16:13:49 +0200

    small change in SeriesInfoRepository

--- src/main/java/se/linda/library/repository/SeriesInfoRepository.java
+++ src/main/java/se/linda/library/repository/SeriesInfoRepository.java
@@ -3,5 +3,5 @@ package se.linda.library.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.linda.library.model.detail.SeriesInfo;
 
-public interface SeriesInfoRepository extends JpaRepository<SeriesInfo, Long> {
+public interface SeriesInfoRepository extends JpaRepository<SeriesInfo, String> {
 }
