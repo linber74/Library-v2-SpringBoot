9a1b36ae628a2c54b282ed6e98c30106496ebf0c
Linda Bergin <linber24@gmail.com>
2026-06-07 16:35:19 +0200

    Created BookService and started with constructor, save, findall, getById and deleteById.

---
+++ src/main/java/se/linda/library/service/BookService.java
@@ -0,0 +1,49 @@
+package se.linda.library.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import se.linda.library.model.entity.Book;
+import se.linda.library.repository.BookRepository;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class BookService {
+
+    final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book save (Book book){
+        if (book == null){
+
+            // Todo Change exceptions later
+            throw new IllegalArgumentException("Book cannot be null");
+        }
+        return bookRepository.save(book);
+    }
+
+    public List <Book> getAll(){
+        return bookRepository.findAll();
+    }
+
+    public Book getById (Long id){
+        return bookRepository.findById(id).orElseThrow(()
+
+            // Todo Change exceptions later
+            -> {return new RuntimeException("Book with id " + id + " not found");} );
+    }
+
+    public void deleteById (Long id){
+        if (!bookRepository.existsById(id)) {
+
+            // Todo Change exceptions later
+            throw new RuntimeException("Book with id " + id + " not found");
+        }
+        bookRepository.deleteById(id);
+    }
+}
