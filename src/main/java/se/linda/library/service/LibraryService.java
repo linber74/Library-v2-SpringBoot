7cca6273a6fde67e14c0bc5dc307a86f6092760d
Linda Bergin <linber24@gmail.com>
2026-08-03 17:10:06 +0200

    Added log.warn and log.info in LibraryService

--- src/main/java/se/linda/library/service/LibraryService.java
+++ src/main/java/se/linda/library/service/LibraryService.java
@@ -1,6 +1,7 @@
 package se.linda.library.service;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import se.linda.library.exception.ItemNotFoundException;
 import se.linda.library.model.detail.SeriesInfo;
@@ -15,6 +16,7 @@ import java.util.Optional;
 
 @Service
 @Transactional
+@Slf4j
 public class LibraryService {
 
     final BookService bookService;
@@ -46,56 +48,64 @@ public class LibraryService {
 
         return libraryItemRepository.findById(id).orElseThrow(()
 
-                // Todo Change exceptions later
-            -> new ItemNotFoundException("Object with id: " + id + " not found!"));
+
+            ->{ log.warn("Object not found with id: {}", id);
+                return new ItemNotFoundException("Object with id: " + id + " not found!");});
     }
 
     public List<LibraryItem> getAllByType (ItemType itemType){
 
         if (itemType == null){
+            log.warn("Item type can't be null!");
             // Todo Change exceptions later
             throw new IllegalArgumentException("Itemtype can't be null!");
         }
-
+        log.info("Getting item type: {}", itemType);
         return libraryItemRepository.findByItemType(itemType);
     }
 
     public List<LibraryItem> getAllByLanguage (String language){
 
         if (language == null || language.isBlank()){
+
+            log.warn("Language can't be null or blank");
             // Todo Change exceptions later
             throw new IllegalArgumentException("Language can't be null or blank");
         }
-
+        log.info("Getting by Language: {}", language);
         return libraryItemRepository.findByLanguage(language);
     }
 
     public List<LibraryItem> getAllByTitle (String title){
 
         if (title == null || title.isBlank()){
+            log.warn("Title can't be null or blank");
             // Todo Change exceptions later
             throw new IllegalArgumentException("Title can't be null or blank");
         }
-
+        log.info("Getting Title: {}", title);
         return libraryItemRepository.findByTitleContainingIgnoreCase(title);
     }
 
     public List<LibraryItem> getAllByPublishYear (Integer year){
 
         if (year < 0){
+            log.warn("Publishingyear can't be 0 or lower");
             // Todo Change exceptions later
             throw new IllegalArgumentException("Publishingyear can't be 0 or lower");
         }
-
+        log.info("Getting from publishing year: {}", year);
         return libraryItemRepository.findByPublishYear(year);
     }
 
     public void deleteById (Long id){
 
         if (!libraryItemRepository.existsById(id)){
+            log.warn(" Object not found with id: {}", id);
             // Todo Change exceptions later
             throw new IllegalArgumentException("Object with id: " + id + " not found");
         }
+        log.info("Deleted object with id: {}", id);
         libraryItemRepository.deleteById(id);
     }
 
