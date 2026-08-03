1b3b68f3b86ee49dbe20ac44da3d4a071b467c9c
Linda Bergin <linber24@gmail.com>
2026-08-03 16:56:38 +0200

    Added log.warn and log.info in GameService

--- src/main/java/se/linda/library/service/GameService.java
+++ src/main/java/se/linda/library/service/GameService.java
@@ -1,6 +1,7 @@
 package se.linda.library.service;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import se.linda.library.exception.ItemNotFoundException;
 import se.linda.library.model.entity.Game;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 @Transactional
+@Slf4j
 public class GameService {
 
     final GameRepository gameRepository;
@@ -21,9 +23,10 @@ public class GameService {
     public Game save (Game game){
         if (game == null){
 
-            // Todo Change exceptions later
+            log.warn("Game can't be null!");
             throw new ItemNotFoundException("Game can't be null!");
         }
+        log.info("Saved Game: {}", game);
         return gameRepository.save(game);
     }
 
@@ -35,25 +38,30 @@ public class GameService {
 
         return gameRepository.findById(id).orElseThrow(()
 
-                // Todo Change exceptions later
-        -> new ItemNotFoundException("Game with id: " + id + " not found!"));
+        -> { log.warn("Game not found With íd: {}", id);
+        return new ItemNotFoundException("Game with id: " + id + " not found!");});
     }
 
     public void deleteById (Long id){
 
         if(!gameRepository.existsById(id)){
 
+            log.warn("Game not found with id: {}", id);
             // Todo Change exceptions later
             throw new IllegalArgumentException("Game with id: " + id + " not found!");
         }
+        log.info("Deleted Game with id: {}",id);
         gameRepository.deleteById(id);
     }
 
     public List<Game> searchByCreator (String creator){
 
         if (creator == null){
+            log.warn("Creator can't be found!");
+            // Todo Change exceptions later
             throw new IllegalArgumentException("Creator can't be found!");
         }
+        log.info("Getting creator: {}", creator);
         return gameRepository.findByCreator(creator);
     }
 }
