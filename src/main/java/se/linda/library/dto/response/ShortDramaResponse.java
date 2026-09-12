c54d3c0256e7a29de13deaa38477cc491a3eae3b
Linda Bergin <linber24@gmail.com>
2026-09-12 22:58:30 +0200

    feat: add ShortDrama and AudioStory request/response DTOs

---
+++ src/main/java/se/linda/library/dto/response/ShortDramaResponse.java
@@ -0,0 +1,7 @@
+package se.linda.library.dto.response;
+
+import java.util.List;
+
+public record ShortDramaResponse(Long id, String title, String language, List<String>tropeGenre,
+Integer durationSeconds) {
+}
